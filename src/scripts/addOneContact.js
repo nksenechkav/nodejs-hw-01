e8294ae214a90339e3a61bd45503ccfe8cb07610
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-05-23 06:14:39 +0200

    add script addOneContact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,25 @@
-// import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { PATH_DB } from '../constants/contacts.js';
 
-// export const addOneContact = async () => {};
+const addOneContact = async () => {
+  try {
+    // Read the current content of the db.json file
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(data);
 
-// await addOneContact();
+    // Generate a new contact
+    const newContact = createFakeContact();
+    contacts.push(newContact);
+
+    // Write the updated array of contacts back to the file
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf8');
+
+    console.log('One new contact has been added successfully.');
+  } catch (error) {
+    console.error('Error adding contact:', error);
+  }
+};
+
+// Execute the addOneContact function when the script is run
+await addOneContact();
