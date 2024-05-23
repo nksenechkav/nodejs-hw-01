b1f0d301a0d0ad8f794ed08f92cbdc52923d7a39
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-05-23 06:23:48 +0200

    add getAllContacts script

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,18 @@
-// import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
 
-// export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+  try {
+    // Read the content of the db.json file
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    // Parse the JSON data to a JavaScript array
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.error('Error reading contacts:', error);
+    return [];
+  }
+};
 
-// console.log(await getAllContacts());
+// Log the result of getAllContacts function
+console.log(await getAllContacts());
