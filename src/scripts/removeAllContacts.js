80b149f498e4c6a2e764014b239bcdf4c85dd789
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-05-25 07:00:15 +0200

    add removeAllContacts.js

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,16 @@
-// import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
 
-// export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    // Write an empty array to the db.json file
+    await fs.writeFile(PATH_DB, JSON.stringify([], null, 2), 'utf8');
+    console.log('All contacts have been removed successfully.');
+  } catch (error) {
+    console.error('Error removing contacts:', error);
+  }
+};
 
-// await removeAllContacts();
+// Execute the removeAllContacts function when the script is run
+await removeAllContacts();
+
