7d4f3592da7f8960ac5eb0248ae58eb2467187ec
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-05-27 05:35:29 +0200

    add fix in contacts.js

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Отримання поточного шляху в ES-модулі
+// Get the current file path in ES module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
