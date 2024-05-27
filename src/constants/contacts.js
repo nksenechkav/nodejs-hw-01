93de44a7d9468695268a01a277d240741be757ef
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-05-27 05:33:10 +0200

    add fix after mentor in constants with module path

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,8 @@
-export const PATH_DB = './src/db/db.json';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Отримання поточного шляху в ES-модулі
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const PATH_DB = path.join(__dirname, '../db/db.json');
